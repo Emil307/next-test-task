e90653174dea3743f3e994441e5ee9ca91fe0098
Emil307 <emilnovikov392@gmail.com>
2024-08-19 00:27:48 +0600

    [Chore] layout content

--- apps/client/src/widgets/Intro/ui/Intro.tsx
+++ apps/client/src/widgets/Intro/ui/Intro.tsx
@@ -1,3 +1,13 @@
 export default () => {
-    return <>Intro</>;
+    return (
+        <div className='flex h-screen items-center justify-center'>
+            <div className='flex h-fit w-2/5 flex-col gap-4'>
+                <h1 className='text-center text-44 font-600'>Make Ship Happen</h1>
+                <p className='text-center text-20'>
+                    Turbo is an incremental bundler and build system optimized for JavaScript and TypeScript, written in
+                    Rust.
+                </p>
+            </div>
+        </div>
+    );
 };
