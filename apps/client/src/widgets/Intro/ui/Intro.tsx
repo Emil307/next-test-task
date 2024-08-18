925d9c45a8d4e58e63372959d0234c55d69f0fe2
Emil307 <emilnovikov392@gmail.com>
2024-08-19 00:39:03 +0600

    [Chore] layout tariffs button

--- apps/client/src/widgets/Intro/ui/Intro.tsx
+++ apps/client/src/widgets/Intro/ui/Intro.tsx
@@ -1,12 +1,15 @@
 export default () => {
     return (
         <div className='flex h-screen items-center justify-center'>
-            <div className='flex h-fit w-2/5 flex-col gap-4'>
+            <div className='flex h-fit w-2/5 flex-col items-center gap-4'>
                 <h1 className='text-center text-44 font-600'>Make Ship Happen</h1>
                 <p className='text-center text-20'>
                     Turbo is an incremental bundler and build system optimized for JavaScript and TypeScript, written in
                     Rust.
                 </p>
+                <button className='h-14 w-64 rounded-8 bg-accent text-20 font-600 text-white transition-colors duration-300 hover:bg-primary'>
+                    Tariffs
+                </button>
             </div>
         </div>
     );
