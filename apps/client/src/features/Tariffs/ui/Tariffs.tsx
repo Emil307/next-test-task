28268998aa584150bc992b45033e454437c5794c
Emil307 <emilnovikov392@gmail.com>
2024-08-19 17:05:07 +0600

    [Feat] tariffs form

--- apps/client/src/features/Tariffs/ui/Tariffs.tsx
+++ apps/client/src/features/Tariffs/ui/Tariffs.tsx
@@ -1,3 +1,43 @@
+'use client';
+
+import type { SubmitHandler } from 'react-hook-form';
+import { useForm } from 'react-hook-form';
+
+interface IFormFileds {
+    tariff: any;
+}
+
 export default () => {
-    return <>Tariffs</>;
+    const { register, handleSubmit } = useForm<IFormFileds>();
+
+    const onSubmit: SubmitHandler<IFormFileds> = data => {
+        console.log(data);
+    };
+
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <input
+                type='radio'
+                {...register('tariff', {
+                    required: 'Обязательное поле',
+                })}
+                value='free'
+            />
+            <input
+                type='radio'
+                {...register('tariff', {
+                    required: 'Обязательное поле',
+                })}
+                value='pro'
+            />
+            <input
+                type='radio'
+                {...register('tariff', {
+                    required: 'Обязательное поле',
+                })}
+                value='enterprice'
+            />
+            <button>submit</button>
+        </form>
+    );
 };
