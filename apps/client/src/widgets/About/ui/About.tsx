56d4d3eabd22ce40e2d939599423b7aa716115d9
Emil307 <emilnovikov392@gmail.com>
2024-08-19 22:50:11 +0600

    [Chore] layout about section

--- apps/client/src/widgets/About/ui/About.tsx
+++ apps/client/src/widgets/About/ui/About.tsx
@@ -1,3 +1,10 @@
 export default () => {
-    return <>About</>;
+    return (
+        <div className='flex items-center justify-center py-32'>
+            <div>
+                <h2 className='text-center text-44 font-600'>We are used by</h2>
+                <p className='text-center text-20'>120 developerss</p>
+            </div>
+        </div>
+    );
 };
